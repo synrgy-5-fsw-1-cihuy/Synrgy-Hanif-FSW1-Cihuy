fc96541888d1b3a3d52c945025b7ad6e24af2742
hanifan-aulia <hanifan.aulia0@gmail.com>
2022-10-12 22:13:16 +0700

    add answer for challenge-03

---
+++ scripts/filterCarByAvailability.js
@@ -0,0 +1,23 @@
+function filterCarByAvailability(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Tempat penampungan hasil
+  const result = [];
+
+  // Tulis code-mu disini
+  // cars.forEach(element => {
+  //   if(element.available == true) {
+  //     result.push(element);
+  //   }
+  // });
+
+  for(let i = 0; i < cars.length; i++) {
+    if(cars[i].available == true) {
+      result.push(cars[i]);
+    }
+  }
+
+  // Rubah code ini dengan array hasil filter berdasarkan availablity
+  return result;
+}
---
+++ scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,36 @@
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+  // for(let i = 0; i < result.length; i++) {
+  //   for(let j = 0; j < result.length - i - 1; j++) {
+  //     if(result[j].year < result[j + 1].year) {
+  //       let tmp = result[j];
+  //       result[j] = result[j + 1];
+  //       result[j + 1] = tmp;
+  //     }
+  //   }
+  // }
+
+  let swapped;
+  do {
+    swapped = false;
+
+    for(let i = 0; i < result.length - 1; i++) {
+      if(result[i].year < result[i + 1].year) {
+        let tmp = result[i];
+        result[i] = result[i + 1];
+        result[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+  } while(swapped)
+
+  // Rubah code ini dengan array hasil sorting secara descending
+  return result;
+}
