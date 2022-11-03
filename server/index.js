5d6f8e03fa439cd4a49c1ef54594633763ffe2d3
hanifan-aulia <hanifan.aulia0@gmail.com>
2022-11-03 22:19:22 +0700

    finishing cari mobil page

--- server/index.js
+++ server/index.js
@@ -1,1 +1,51 @@
-console.log("Implement servermu disini yak 😝!");
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const PUBLIC_DIR = path.join(__dirname, '../public');
+const {PORT = 8000} = process.env;
+
+function htmlReaderFile(htmlFileName) {
+    const htmlFilePath = path.join(PUBLIC_DIR, htmlFileName);
+    const readHtmlFile = fs.readFileSync(htmlFilePath, 'utf-8');
+
+    return readHtmlFile;
+}
+
+// Request response handler
+function onRequest(request, response) {
+    switch (request.url) {
+        case "/":
+            response.writeHead(200);
+            response.end(htmlReaderFile('index.html'));
+            return;
+
+        case "/about":
+            response.writeHead(200);
+            response.end(htmlReaderFile('about.html'));
+            return;
+
+        case "/profile":
+            response.writeHead(200);
+            response.end(htmlReaderFile('profile.html'));
+            return;
+
+        case "/contact-us":
+            response.writeHead(200);
+            response.end(htmlReaderFile('contact-us.html'));
+            return;
+
+        default:
+            response.writeHead(404);
+            response.end(htmlReaderFile('404.html'));
+            return;
+    }
+
+}
+
+// Server instance
+const server = http.createServer(onRequest);
+
+// Listen portnya
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at localhost:${PORT}`);
+})
