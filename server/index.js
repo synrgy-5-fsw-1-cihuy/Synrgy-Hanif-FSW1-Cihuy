02cdcce7a13ae751b2d1b3839d958eb5507c32b3
hanifan-aulia <hanifan.aulia0@gmail.com>
2022-11-05 11:44:54 +0700

    setup http server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const PUBLIC_DIR = path.join(__dirname, '../public');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+const static = require("node-static");
+const PUBLIC_DIR = path.join(__dirname, "../public");
+const fileStatic = new static.Server(PUBLIC_DIR);
 const {PORT = 8000} = process.env;
 
 function htmlReaderFile(htmlFileName) {
     const htmlFilePath = path.join(PUBLIC_DIR, htmlFileName);
-    const readHtmlFile = fs.readFileSync(htmlFilePath, 'utf-8');
+    const readHtmlFile = fs.readFileSync(htmlFilePath, "utf-8");
 
     return readHtmlFile;
 }
@@ -16,27 +19,46 @@ function onRequest(request, response) {
     switch (request.url) {
         case "/":
             response.writeHead(200);
-            response.end(htmlReaderFile('index.html'));
+            response.end(htmlReaderFile("index.html"));
             return;
 
-        case "/about":
+        case "/cari-mobil":
             response.writeHead(200);
-            response.end(htmlReaderFile('about.html'));
-            return;
-
-        case "/profile":
-            response.writeHead(200);
-            response.end(htmlReaderFile('profile.html'));
-            return;
-
-        case "/contact-us":
-            response.writeHead(200);
-            response.end(htmlReaderFile('contact-us.html'));
+            response.end(htmlReaderFile("cari-mobil.html"));
             return;
 
         default:
-            response.writeHead(404);
-            response.end(htmlReaderFile('404.html'));
+            const fileTypes = {
+                css: "text/css",
+                js: "application/javascript",
+                ico: "image/x-icon",
+                png: "image/png",
+                jpg: "image/jpeg",
+                jpeg: "image/jpeg",
+                svg: "image/svg+xml",
+                json: "application/json",
+                map: "application/json",
+                txt: "text/plain",
+            };
+
+            const pathname = url.parse(request.url, true).pathname;
+            
+            fs.readFile("./public" + pathname, (err, file) => {
+                if (err) {
+                    response.status = 404;
+                    response.end("404 Not Found");
+                    return;
+                }
+    
+                for (const [key] of Object.entries(fileTypes)) {
+                    const end = `.${key}`;
+                    if (request.url.endsWith(end)) {
+                        response.setHeader("Content-Type", fileTypes[key]);
+                        response.end(file);
+                        return;
+                    }
+                }
+            });
             return;
     }
 
@@ -46,6 +68,6 @@ function onRequest(request, response) {
 const server = http.createServer(onRequest);
 
 // Listen portnya
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, 'localhost', () => {
     console.log(`Server running at localhost:${PORT}`);
 })
