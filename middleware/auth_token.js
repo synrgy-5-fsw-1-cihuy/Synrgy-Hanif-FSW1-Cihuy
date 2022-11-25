c6c49551a051b4591d8012cf3d76ee828a04922e
hanifan-aulia <hanifan.aulia0@gmail.com>
2022-11-25 21:55:41 +0700

    authtoken middleware

--- middleware/auth_token.js
+++ middleware/auth_token.js
@@ -1,3 +1,28 @@
-function auth_token () {
-    
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+function authToken (req, res, next) {
+    console.log(req.headers);
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.status(401).json({error: "Unauthorized access"});
+
+    try {
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+        if (decoded == null) {
+            res.status(401).json({error: "Unautorized access"});
+        };
+
+        next();
+    } catch(err) {
+        res.status(401).json({error: "Unauthorized access"});
+        throw err;
+        return;
+    };
 }
+
+module.exports = authToken;
--- router/user.router.js
+++ router/user.router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controller/user.controller.js");
+const authMiddleware = require('../middleware/auth_token.js');
 
 // POST
 router.post('/api/auth/register', userController.registerUser);
 
-router.post('/api/auth/login', userController.loginHandler);
+router.post('/api/auth/login', authMiddleware ,userController.loginHandler);
 
 module.exports = router;
