3de5d262515a3e3afcc30539ed6f4dad69b340ba
hanifan-aulia <hanifan.aulia0@gmail.com>
2022-11-29 21:49:45 +0700

    swagger init

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const formidableMiddleware = require("formidable");
 
 const carRouter = require("./router/car.router.js");
@@ -6,6 +8,38 @@ const userRouter = require('./router/user.router.js');
 
 const PORT = 8005 || process.env.PORT;
 const app = express();
+
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Binar Synrgy Express API with Swagger",
+        version: "0.1.0",
+        description:
+          "This is a simple CRUD API application made with Express and documented with Swagger",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "Binar Synrgy",
+          url: "https://binaracademy.com",
+          email: "binaracademy.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000/posts",
+        },
+      ],
+    },
+    apis: [
+        "./router/car.router.js"
+    ],
+};
+const specs = swaggerJsDoc(options);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(carRouter);
 app.use(userRouter);
