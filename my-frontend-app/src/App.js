43323e05c72df62114d15a87a7a1f8fd62528a4f
hanifan-aulia <hanifan.aulia0@gmail.com>
2022-12-08 22:12:19 +0700

    react router dom

--- my-frontend-app/src/App.js
+++ my-frontend-app/src/App.js
@@ -1,18 +1,49 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
 import Content from './components/content/Content';
 import Footer from './components/footer/Footer';
+import Contact from './components/contact/Contact';
+import Profile from './components/profile/Profile';
 import FAQ from './faq/FAQ';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './components/about/About';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { useState } from 'react';
 
 function App() {
   const person = {
     name: "Roger"
   };
-  
+
+  const [page, setPage] = useState();
+
   return (
     
     <div className="App">
+
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={() => setPage("")} href={`http://localhost:3000/${page}`}>Home</Nav.Link>
+            <Nav.Link onClick={() => setPage("contact")} href={`http://localhost:3000/${page}`}>Contact</Nav.Link>
+            <Nav.Link onClick={() => setPage("about")} href={`http://localhost:3000/${page}`}>About</Nav.Link>
+            <Nav.Link onClick={() => setPage("profile")} href={`http://localhost:3000/${page}`}>Profile</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Router>
+        <Routes>
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </Router>
+
       <Header />
       <Content 
       name="Zaki"
@@ -20,7 +51,8 @@ function App() {
       />
       <FAQ />
       <Footer />
-      <header className="App-header">
+
+      {/* <header className="App-header">
       
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -35,7 +67,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ my-frontend-app/src/components/profile/Profile.js
@@ -0,0 +1,9 @@
+const Profile = () => {
+    return(
+        <div>
+            <h2>Profile component is works!</h2>
+        </div>
+    );
+};
+
+export default Profile;
