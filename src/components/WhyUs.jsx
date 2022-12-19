605d4fd8f6c0adccac30ef5c302bb6b4206e16e5
hanifan-aulia <hanifan.aulia0@gmail.com>
2022-12-19 16:06:34 +0700

    create filter for cars

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -6,26 +6,26 @@ const WhyUs = () => {
             <Container>
                 <Row className="mt-5 mb-2 pt-5 py-2">
                     <Col md={4} className="ms-4 ms-md-5 ps-5">
-                        <p class="text-title fs-4">Why Us?</p>
-                        <p class="text-content">Mengapa harus pilih Binar Car Rental?</p>
+                        <p className="text-title fs-4">Why Us?</p>
+                        <p className="text-content">Mengapa harus pilih Binar Car Rental?</p>
                     </Col>
                 </Row>
                 <Row className="mx-5 mt-2 mb-5 px-3 gy-3">
                     <Col md={3}>
                         <Card className="h-100">
                             <Card.Body>
-                                <img src="./assets/img/icon_thumbup.png" alt="complete" class="mt-2 ms-2"/>
-                                <h5 class="text-subtitle my-3 ms-2 fw-bold">Mobil Lengkap</h5>
-                                <p class="text-content ms-2 me-3">Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat</p>
+                                <img src="./assets/img/icon_thumbup.png" alt="complete" className="mt-2 ms-2"/>
+                                <h5 className="text-subtitle my-3 ms-2 fw-bold">Mobil Lengkap</h5>
+                                <p className="text-content ms-2 me-3">Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat</p>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="h-100">
                             <Card.Body>
-                                <img src="./assets/img/icon_price.png" alt="complete" class="mt-2 ms-2"/>
-                                <h5 class="text-subtitle my-3 ms-2 fw-bold">Harga Murah</h5>
-                                <p class="text-content ms-2 me-3">Harga murah dan bersaing, bisa bandingkan harga kami dengan
+                                <img src="./assets/img/icon_price.png" alt="complete" className="mt-2 ms-2"/>
+                                <h5 className="text-subtitle my-3 ms-2 fw-bold">Harga Murah</h5>
+                                <p className="text-content ms-2 me-3">Harga murah dan bersaing, bisa bandingkan harga kami dengan
                                 rental mobil lain</p>
                             </Card.Body>
                         </Card>
@@ -33,9 +33,9 @@ const WhyUs = () => {
                     <Col md={3}>
                         <Card className="h-100">
                             <Card.Body>
-                                <img src="./assets/img/icon_24hrs.png" alt="complete" class="mt-2 ms-2"/>
-                                <h5 class="text-subtitle my-3 ms-2 fw-bold">Layanan 24 Jam</h5>
-                                <p class="text-content ms-2 me-3">Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga
+                                <img src="./assets/img/icon_24hrs.png" alt="complete" className="mt-2 ms-2"/>
+                                <h5 className="text-subtitle my-3 ms-2 fw-bold">Layanan 24 Jam</h5>
+                                <p className="text-content ms-2 me-3">Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga
                                 tersedia di akhir minggu</p>
                             </Card.Body>
                         </Card>
@@ -43,9 +43,9 @@ const WhyUs = () => {
                     <Col md={3}>
                         <Card className="h-100">
                             <Card.Body>
-                                <img src="./assets/img/icon_professional.png" alt="complete" class="mt-2 ms-2"/>
-                                <h5 class="text-subtitle my-3 ms-2 fw-bold">Sopir Profesional</h5>
-                                <p class="text-content ms-2 me-3">Sopir yang profesional, berpengalaman, jujur, ramah dan selalu
+                                <img src="./assets/img/icon_professional.png" alt="complete" className="mt-2 ms-2"/>
+                                <h5 className="text-subtitle my-3 ms-2 fw-bold">Sopir Profesional</h5>
+                                <p className="text-content ms-2 me-3">Sopir yang profesional, berpengalaman, jujur, ramah dan selalu
                                 tepat waktu</p>
                             </Card.Body>
                         </Card>
